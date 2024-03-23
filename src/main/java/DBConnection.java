832b9d70a65f2ebbf4ae5390e17c5a255de18c43
Chelsea <chelsea.ren.b@gmail.com>
2024-03-23 15:25:01 -0400

    fixed getconnection()

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -3,21 +3,31 @@ import java.sql.*;
 public class DBConnection {
 
     public static void main(String[] args) {
-        try {
-            Connection db = DBConnection.getConnection();
-
+        try{
+            Connection db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "RariJackAppleDash");
             Statement st = db.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM Chain");
-
-            while (rs.next()) {
-                System.out.println(rs.getString(1));
+            ResultSet rs = st.executeQuery("SELECT * FROM \"Hotel\".chain");
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int numOfColumns = rsmd.getColumnCount();
+            System.out.println("number of columns: "+numOfColumns);
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                System.out.print(rsmd.getColumnName(i));
+                System.out.print("\t\t\t");
+            }
+            System.out.println();
+            while(rs.next()){
+                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                    System.out.print(rs.getString(i));
+                    System.out.print("\t");
+                }
+                System.out.println();
             }
             rs.close();
             st.close();
-            db.close();
-        } catch(Exception e) {
-            System.out.println("Exception");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
     private static final String ipAddress = "127.0.0.1";
     private static final String dbServerPort = "5432";
@@ -40,7 +50,8 @@ public class DBConnection {
             con = DriverManager.getConnection("jdbc:postgresql://"
                     + ipAddress + ":" + dbServerPort + "/" + dbName, dbusername, dbpassword);
             return con;
-        } catch (Exception e) {
+        } catch (Throwable e) {
+            System.out.println("connection error: "+e.getMessage());
 
             throw new Exception("Could not establish connection with the Database Server: "
                     + e.getMessage());
@@ -59,6 +70,7 @@ public class DBConnection {
             if (con != null)
                 con.close();
         } catch (SQLException e) {
+            System.out.println("sql error: "+e.getMessage());
             throw new SQLException("Could not close connection with the Database Server: "
                     + e.getMessage());
         }
